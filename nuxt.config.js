76bc2cd4eb10e91c71c6eed97e9e5fff311d59c1
Dominik Hofer <hfrdmnk@gmail.com>
2021-07-24 20:59:01 +0200

    added desc

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'linea is an independent product studio. it focuses on minimalist apps and other creative projects.' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
